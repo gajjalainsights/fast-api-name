d8f7d86b03545de4f7b359d603a7015eb68c5436
Prasanthi Gajjala <155913142+gajjalainsights@users.noreply.github.com>
2024-02-04 19:11:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI(
     title="Name - FastAPI",
-    description="Name file Fast API ",
-)
+    description="Name file Fast API ")
 
 @app.get("/")
 async def name():
